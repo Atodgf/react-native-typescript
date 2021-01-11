d89e88e61f2551e5aa555312ace4233395bdb30a
fgdota2 <68871109+fgdota2@users.noreply.github.com>
2021-01-11 10:29:50 +0300

    small updates

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,43 +1,10 @@
-import React, { FC, useState, useEffect } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import { Input, Button } from '../components'
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-import * as Location from 'expo-location';
+import React, { FC,  } from 'react'
+import { View,  StyleSheet } from 'react-native'
+import {  Button } from '../components'
+
 
 const signUp : FC = (props:any) => {
-    // const [location, setLocation] = useState<any>(null);
-    // const [errorMsg, setErrorMsg] = useState<any>(null);
     
-    // useEffect(() => {
-    //     (async () => {
-    //       let { status } = await Location.requestPermissionsAsync();
-    //       if (status !== 'granted') {
-    //         setErrorMsg('Permission to access location was denied');
-    //         return;
-    //       }
-    
-    //       let location = await Location.getCurrentPositionAsync({});
-    //       setLocation(location);
-    //     })();
-    //   }, []);
-
-
-
-    // const MapButton = () =>{
-    //     return(
-    //         <MapView
-    //             provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-    //             style={styles.map}
-    //             region={{
-    //                 latitude: location.coords.latitude,
-    //                 longitude: location.coords.longitude,
-    //                 latitudeDelta: 0.015,
-    //                 longitudeDelta: 0.0121,
-    //             }}
-    //             >
-    //      </MapView> 
-    //     )
-    // }  
     return (
         <View style={styles.container}>
             <Button title="Map" onPress={()=> props.navigation.navigate('map')}/>
